b0bfdbaa3c411b75b60830c641a44b568908c669
foriask <Foriask16@gmail.com>
2025-12-08 12:52:06 +0100

    help maybe done

--- naiad_main.lua
+++ naiad_main.lua
@@ -27,8 +27,50 @@ local commands = {
 		-- This can be just a standalone function
 		args_number = 0, -- how many words after this are passed to "to_do" section
 	},
+	help = {
+		-- Help needs to be a special command, with this name and definition, cause it's called in the main function (just to save time)
+		format = {
+			first = "Naiad is used with the commands below, followed by their respective arguments. ",
+			last = "",	
+			separator_one = " >> ",
+			endline_one = "\n",
+			endline_two = "\n",
+			endline_three = "\n\n",
+			per_command = function(_cmmd, _format)
+				print(_format.endline_two .. _cmmd.name .. _format.separator_one .. _cmmd.desc)
+			end,
+		},
+		all = function(_format, _commands)
+			print(_format.first .. _format.endline_one)
+			for _i, _cmmd in ipairs(_commands) do
+				per_command(_cmmd, _format)
+			end
+			print(_format.endline_three .. _format.last),
+		end,
+	},
 }
 
+--[[
+	Error cheatsheet:
+	2 -> No command found, "help" is called
+	3 -> Not enough arguments, "help _cmmd" is called
+
+
+
+]]
+
 local function working_naiad(_args, _commands)
-
-
+	for _i, cmmd in ipairs(_commands) do -- Just iterate in the big command section
+		if not ((_cmmd.command == _args[1]) or (_commd.command == _args[1])) then
+			-- This checks if args (in this case, all of the command) coincides with one of the commands, if true, keep cheking until know what to do
+			_commands.help.all()
+			os.exit(2)
+			return
+		end
+		if not (_cmmd.args_number == #_args) then
+			_commands.help.this(_cmmd)
+			os.exit(3)
+			return
+		end
+	end
+end
